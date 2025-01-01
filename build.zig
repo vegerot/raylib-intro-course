c213cacea92a3597f92e60c30df94eb0938d04b8
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2024-12-31 16:20:34 -0800

    me: rename to bricks

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const exe = b.addExecutable(.{
-        .name = "raylib-zig",
+        .name = "bricks",
         .root_source_file = b.path("me/game.zig"),
         .target = target,
     });
